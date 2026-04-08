07ae6c6594ac08689fcacc2232c730fe170d95b6
keegan boutwell <boutwellke07@gmail.com>
2026-04-08 10:51:48 -0400

    Working on sql

---
+++ app/src/main/java/com/example/picktick/SQLiteHelper.kt
@@ -0,0 +1,10 @@
+package com.example.picktick
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+class SQLiteHelper{
+    object MainDataBase : BaseColumns {
+        const val TABLE_NAME = "MainDataBase"
+    }
+    object UserDB
+    //fun CreateNewDataBase()
+}
